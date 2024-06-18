b49e8bff0dcd7fe8a3d00cfd8692e26da71995fc
teddybear0430 <teddybear0430@hotmail.com>
2024-06-19 00:34:06 +0900

    feat: 大枠を作って。

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  tabWidth: 2,
+  semi: false,
+}
+
+module.exports = config
