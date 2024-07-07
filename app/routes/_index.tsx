c01bb08cd8c175f553c8c85eb2e10f5ed10168bf
teddybear0430 <teddybear0430@hotmail.com>
2024-07-08 00:42:51 +0900

    fix: undo cache-control.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -8,7 +8,11 @@ import type { HeadersFunction } from '@remix-run/cloudflare'
 export const headers: HeadersFunction = () => {
   return {
     // TODO: キャッシュ戦略後でちゃんと考える
-    'Cache-Control': 'max-age=0, s-maxage=60, stale-while-revalidate=60',
+    // - レスポンスは7日間（604800 秒間）は新鮮。
+    // - 7日間経過したら、そこから 360s は、古いキャッシュを参照するが、バックグラウンドでfetchを行い、キャッシュを更新して、サーバから新しいリソースを取得できたら、キャッシュを更新する。
+    // - 7日を過ぎると古くなるが、サーバーがエラーでレスポンスを返した場合はさらに1日（86400 秒間）利用できる。
+    'Cache-Control':
+      'max-age=0, s-maxage=604800, stale-while-revalidate=360, stale-if-error=86400',
   }
 }
 
